02442b33cbd8a0113b128a8a0e559418bb0f4dd1
sid2033 <57922728+sid2033@users.noreply.github.com>
2020-12-26 01:15:02 +0530

    Add files via upload

---
+++ videoplayer.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() { 
+    startPlayer(); 
+});
+
+let player;
+
+function startPlayer() {
+    player = document.getElementById("player");
+    player.controls = false;
+}
+
+function play_sound() {
+    player.play();
+}
+
+function pause_sound() {
+    player.pause();
+}
+
+function stop_sound() {
+    player.pause();
+    player.currentTime = 0;
+}
+
+function change_volume() {
+    player.volume = document.getElementById("change_volume").value;
+}
+
+function changeSRC() {
+    let file = document.getElementById('vidInput').files;
+    const objectURL = URL.createObjectURL(file[0]);
+    player.src = objectURL; 
+    player.play();
+}
+
+
